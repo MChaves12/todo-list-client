8cbd948ff477d55ebf8df133ec1c80730cfe4638
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-05-23 10:10:56 -0300

    Private and Anon Routes created and implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import IsPrivate from './components/IsPrivate';
+import IsAnon from './components/IsAnon';
 
 function App() {
 
@@ -7,6 +10,9 @@ function App() {
       <div>
         <Routes>
           <Route path='/' element={<div>Todo List</div>}></Route>
+          <Route to='/task' element={<IsPrivate><TaskPage /></IsPrivate>} />
+          <Route path='/signup' element={<IsAnon><SignupPage /></IsAnon>} />
+          <Route path='/login' element={<IsAnon><LoginPage /></IsAnon>} />
         </Routes>
       </div>
   );
